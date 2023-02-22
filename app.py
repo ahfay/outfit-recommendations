ac1e32315535242c9b3f9bb06a46a9ef2346e348
ahfay <122598374+ahfay@users.noreply.github.com>
2023-02-23 00:31:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,6 @@ st.write("Sistem ini akan menghasilkan rekomendasi outfit yang cocok berdasarkan
 col1, col2, col3 = st.columns(3)
 col2.image(image_ex, caption="Contoh Input Gambar")
 
-
-file = st.file_uploader('', type='jpg')
-if file is not False:
-    pass
-else:
-    st.warning('HANYA BISA FOTO YANG BERFORMAT JPG')
-
-
 def prediksi_outfit(image, model):
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img,(126,224))/255
@@ -79,15 +71,20 @@ def predik_wajah(gbr):
     list_hasil.append(contoh)
     return list_hasil 
 
-proses = predik_wajah(file)
-wajah = proses[0]
-outfit = proses[1]
-caption = proses[2]
-contoh_ = proses[3]
-st.header("Hasil Rekomendasi Outfit")
-cola, colb, colc = st.columns(3)
-img_ = Image.open('assets/crop_face.jpg')
-cola.image(img_, caption=wajah)
-img__ = Image.open(contoh_)
-colc.image(img__, caption=outfit)
-st.success(caption)
+file = st.file_uploader('', type='jpg')
+if file is not False:
+    proses = predik_wajah(file)
+    wajah = proses[0]
+    outfit = proses[1]
+    caption = proses[2]
+    contoh_ = proses[3]
+    st.header("Hasil Rekomendasi Outfit")
+    cola, colb, colc = st.columns(3)
+    img_ = Image.open('assets/crop_face.jpg')
+    cola.image(img_, caption=wajah)
+    img__ = Image.open(contoh_)
+    colc.image(img__, caption=outfit)
+    st.success(caption)
+else:
+    st.warning('HANYA BISA FOTO YANG BERFORMAT JPG')
+
